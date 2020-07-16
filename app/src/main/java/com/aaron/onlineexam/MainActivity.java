e3fe0d63c677e54af5480a99a264eca65f866293
aaronleegit <10851022@st.fhjh.tp.edu.tw>
2020-07-16 11:24:37 +0800

    Added Name

--- app/src/main/java/com/aaron/onlineexam/MainActivity.java
+++ app/src/main/java/com/aaron/onlineexam/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+//This is Aaron Lee's Project
 public class MainActivity extends AppCompatActivity {
 
     @Override
